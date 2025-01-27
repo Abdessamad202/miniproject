8fe33771bb93eb72b50aed434cb1135808dacf65
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-01-27 14:56:55 +0100

    Add Vite configuration file

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    allowedHosts: [
+      '7642-196-117-62-102.ngrok-free.app', // Replace this with the ngrok host
+    ],
+  },
+});
