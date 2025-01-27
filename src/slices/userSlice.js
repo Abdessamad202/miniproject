ec3344a604799722da9852197e8311a11412d9d1
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-01-27 14:56:58 +0100

    Add userSlice for managing user state

---
+++ src/slices/userSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    id: null,
+    name: '',
+    email: '',
+    isLoggedIn: false,
+  },
+  reducers: {
+    In: (state, action) => {
+      const { id, name, email } = action.payload.user;
+      state.id = id;
+      state.name = name;
+      state.email = email;
+      state.isLoggedIn = true;
+    },
+    Out: (state) => {
+      state.id = null;
+      state.name = '';
+      state.email = '';
+      state.isLoggedIn = false;
+    },
+  },
+});
+
+export const { In, Out } = userSlice.actions;
+export { userSlice };
