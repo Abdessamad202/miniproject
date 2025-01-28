0597e2fade68f88cb69f428bcb9a979de24e8bba
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-01-28 23:52:55 +0100

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { userSlice } from "../slices/userSlice";
+import { usersSlice } from "../slices/usersSlice";
 
 const store = configureStore({
   reducer : {
-    user : userSlice.reducer
+    user : userSlice.reducer,
+    users : usersSlice.reducer
   }
 })
 export {store}
