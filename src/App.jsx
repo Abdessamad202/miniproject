70f2f5529c29247285b8b3b5f3be149538c50863
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-01-28 17:59:29 +0100

    Add App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,25 @@ import { Routes } from "react-router"
 import Login from "./components/Login"
 import Register from "./components/Register"
 import { Home } from "./components/Home"
+import { useState } from "react"
 
 const App = ()=> {
+  const [state, setstate] = useState();
+  const handleclick = () => {
+    const token = window.sessionStorage.getItem("token")
+    setstate(token)
+  }
   return (
+    <>
+      <button type="button" onClick={handleclick}>token</button>
+      {state}
     <Routes>
       <Route path="/" element={<Home />}></Route>
       <Route path="/login" element={<Login/>}></Route>
       <Route path="register" element={<Register />}></Route>
     </Routes>
+
+    </>
 
   )
 }
